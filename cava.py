222d6e403f1f978e1101b3a55e7fddc5d70e905b
ludinhquan <ludinhquan@gmail.com>
2022-08-03 20:21:08 +0700

    update led

--- cava.py
+++ cava.py
@@ -4,7 +4,10 @@ import subprocess
 import tempfile
 import math
 import numpy as np
+import time
+from rpi_ws281x import PixelStrip, Color
 
+# CAVA configuration
 BARS_NUMBER = 20
 OUTPUT_BIT_FORMAT = "16bit"
 RAW_TARGET = "/dev/stdout"
@@ -21,6 +24,17 @@ bit_format = %s
 
 config = conpat % (BARS_NUMBER, RAW_TARGET, OUTPUT_BIT_FORMAT)
 bytetype, bytesize, bytenorm = ("H", 2, 65535) if OUTPUT_BIT_FORMAT == "16bit" else ("B", 1, 255)
+
+
+# LED strip configuration:
+LED_COUNT = 16        # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 def run():
     with tempfile.NamedTemporaryFile() as config_file:
@@ -46,5 +60,21 @@ def run():
             sample = [str(math.floor(i / bytenorm  * 10)) for i in struct.unpack(fmt, data)]
             print(";".join(sample))
     
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+def initLedStrip():
+    # Create NeoPixel object with appropriate configuration.
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+    return strip
+
 if __name__ == "__main__":
-    run()
+    strip = initLedStrip()
+    # run()
