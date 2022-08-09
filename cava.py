fcc19d2c8e0f9de79b7e63bfacf4cdf435523563
ludinhquan <ludinhquan@gmail.com>
2022-08-09 16:33:21 +0100

    show led

--- cava.py
+++ cava.py
@@ -8,28 +8,36 @@ import time
 from rpi_ws281x import PixelStrip, Color
 
 # CAVA configuration
-BARS_NUMBER = 20
-OUTPUT_BIT_FORMAT = "16bit"
+BARS_NUMBER = 12
+BARS_HEIGHT = 10
 RAW_TARGET = "/dev/stdout"
+OUTPUT_BIT_FORMAT = "16bit"
 RATE = 44100
 
 conpat = """
 [general]
 bars = %d
+
 [output]
 method = raw
-raw_target = %s
-bit_format = %s
+channels = mono
+raw_target = /dev/stdout
+data_format = ascii
+bit_format = 16bit
+ascii_max_range = %d
+
+[smoothing]
+monstercat = 1
+waves = 1
+noise_reduction = 0.3
 """
 
-config = conpat % (BARS_NUMBER, RAW_TARGET, OUTPUT_BIT_FORMAT)
-bytetype, bytesize, bytenorm = ("H", 2, 65535) if OUTPUT_BIT_FORMAT == "16bit" else ("B", 1, 255)
-
+config = conpat % (BARS_NUMBER, BARS_HEIGHT)
 
 # LED strip configuration:
-LED_COUNT = 16        # Number of LED pixels.
-LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
-# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_COUNT = BARS_NUMBER * BARS_HEIGHT        # Number of LED pixels.
+#LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
@@ -42,43 +50,34 @@ def run():
         config_file.flush()
         
         process = subprocess.Popen(["cava", "-p", config_file.name], stdout=subprocess.PIPE)
-        chunk = bytesize * BARS_NUMBER
-        fmt = bytetype * BARS_NUMBER
-        
-        if RAW_TARGET != "/dev/stdout":
-            if not os.path.exists(RAW_TARGET):
-                os.mkfifo(RAW_TARGET)
-            source = open(RAW_TARGET, "rb")
-        else:
-            source = process.stdout
-        
+        source = process.stdout
 
         while True:
-            data = source.read(chunk)
-            if len(data) < chunk:
-                break
-            sample = [str(math.floor(i / bytenorm  * 10)) for i in struct.unpack(fmt, data)]
-            print(";".join(sample))
-    
-# Define functions which animate LEDs in various ways.
-def colorWipe(strip, color, wait_ms=50):
-    """Wipe color across display a pixel at a time."""
+            line = source.readline()
+            if not line:
+                break;
+            data = line.decode('UTF-8').rstrip().split(';')[:-1]
+            sample = [int(i) for i in data]
+            showLed(sample)
+            print(sample) 
+
+def resetLed():
+    color = Color(0,0,0)
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, color)
-        strip.show()
-        time.sleep(wait_ms / 1000.0)
 
-def initLedStrip():
-    # Create NeoPixel object with appropriate configuration.
-    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-    # Intialize the library (must be called once before other functions).
-    strip.begin()
-    return strip
+def showLed(sample):
+    if len(set(sample)) == 1:
+        return  
 
+    resetLed();
+    color = Color(0, 1, 1);
+    for i, height in enumerate(sample):
+        for j in range(height):
+            strip.setPixelColor(i * BARS_HEIGHT + j, color)
+    strip.show() 
+
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 if __name__ == "__main__":
-    strip = initLedStrip()
-    print('Color wipe animations.')
-    colorWipe(strip, Color(255, 0, 0))  # Red wipe
-    colorWipe(strip, Color(0, 255, 0))  # Green wipe
-    colorWipe(strip, Color(0, 0, 255))  # Blue wipe
-    # run()
+    strip.begin()
+    run();
